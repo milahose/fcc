00e8e56ffd5c6375ba38daef554e469623589921
Mila Hose <mila.hose@gmail.com>
2017-10-26 15:20:42 -0700

    Add loading functionality

--- front-end/intermediate/local-weather/index.js
+++ front-end/intermediate/local-weather/index.js
@@ -1,9 +1,3 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success, error);
-} else {
-  alert('Geolocation is not supported');
-}
-
 const error = () => {
   alert('Unable to determine current location');
 }
@@ -12,53 +6,36 @@ const displayWeather = (position) => {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
 
-  const API = `http://api.wunderground.com/api/${key}/forecast/geolookup/conditions/q/${lat},${lon}.json`
+  const key = '407df37516d6efce';
+  const API = `http://api.wunderground.com/api/${key}/conditions/forecast/forecast10day/q/${lat},${lon}.json`;
 
-  $.getJSON(API, (data) => {
-    $('.weather').html(JSON.stringify(data));
+  $.getJSON(API, (weather) => {
+    $('h1').html('Local Weather');
+    $('h2').html(weather.current_observation.display_location.full);
+    $('p').html(weather.current_observation.weather);  
+    $('.fahrenheit').html(`${Math.ceil(weather.current_observation.temp_f)} &deg;<a href="#">F</a>`);
+    $('.celsius').html(`${Math.ceil(weather.current_observation.temp_c)} &deg;<a href="#">C</a>`);
+    $('a').on('click', () => { $('h2').toggle() });
+
+  if (weather.current_observation.weather === 'Clear' || weather.current_observation.weather === 'Dust') {
+      $('.weather_icon').html('<div class="icon sunny"><div class="sun"><div class="rays"></div></div></div>');
+    } else if (weather.current_observation.weather === 'Clouds' || weather.current_observation.weather === 'Haze') {
+      $('.weather_icon').html('<div class="icon cloudy"><div class="cloud"></div><div class="cloud"></div></div>');
+    } else if (weather.current_observation.weather === 'Thunderstorm') {
+      $('.weather_icon').html('<div class="icon thunder-storm"><div class="cloud"></div><div class="lightning"><div class="bolt"></div><div class="bolt"></div></div></div>');
+    } else if (weather.current_observation.weather === 'Snow') {
+      $('.weather_icon').html('<div class="icon flurries"><div class="cloud"></div><div class="snow"><div class="flake"></div><div class="flake"></div></div></div>');
+    } else if (weather.current_observation.weather === 'Rain') {
+      $('.weather_icon').html('<div class="icon rainy"><div class="cloud"></div><div class="rain"></div></div>');
+    } else if (weather.current_observation.weather === 'Drizzle') {
+      $('.weather_icon').html('<div class="icon sun-shower"><div class="cloud"></div><div class="sun"><div class="rays"></div></div><div class="rain"></div></div>');
+    }
   });
 }
 
-// if (navigator.geolocation) {
-//   navigator.geolocation.watchPosition((position) => {
-//     displayWeather(position.coords.latitude, position.coords.longitude);
-//   });
-// }
-
-// const displayWeather = (latitude, longitude) => {
-//   let lat = latitude;
-//   let lon = longitude;
-
-//   console.log('Latitude: ', lat);
-//   console.log('Longitude: ', lon);
-
-//   $.getJSON(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`, (data) => {
-//     // Show weather for current location data.main.temp, data.main.humidity
-//     // Different icon or background image depending on weather
-//     // Push a button to toggle between F/C
-
-//     // $('.weather').html(JSON.stringify(data));
-//     $('.weather').append('<h1>' + data.name + ', ' + data.sys.country + '</h1>');
-//     $('.weather').append('<p>' + data.weather[0].main + '</p>');
-//     $('.weather').append('<h2 style="display: none">' + Math.round(JSON.stringify(data.main.temp + 9)) + ' &deg;<a href="#">C</a></h2>');
-//     $('.weather').append('<h2>' + Math.floor(JSON.stringify((data.main.temp * 9) / 5 + 38)) + ' &deg;<a href="#">F</a></h2>');
-//     $('a').on('click', () => {
-//       $('h2').toggle();
-//     });
-
-//     if (data.weather[0].main === 'Clear' || data.weather[0].main === 'Dust') {
-//       $('.weather').append('<div class="icon sunny"><div class="sun"><div class="rays"></div></div></div>');
-//     } else if (data.weather[0].main === 'Clouds' || data.weather[0].main === 'Haze') {
-//       $('.weather').append('<div class="icon cloudy"><div class="cloud"></div><div class="cloud"></div></div>');
-//     } else if (data.weather[0].main === 'Thunderstorm') {
-//       $('.weather').append('<div class="icon thunder-storm"><div class="cloud"></div><div class="lightning"><div class="bolt"></div><div class="bolt"></div></div></div>');
-//     } else if (data.weather[0].main === 'Snow') {
-//       $('.weather').append('<div class="icon flurries"><div class="cloud"></div><div class="snow"><div class="flake"></div><div class="flake"></div></div></div>');
-//     } else if (data.weather[0].main === 'Rain') {
-//       $('.weather').append('<div class="icon rainy"><div class="cloud"></div><div class="rain"></div></div>');
-//     } else if (data.weather[0].main === 'Drizzle') {
-//       $('.weather').append('<div class="icon sun-shower"><div class="cloud"></div><div class="sun"><div class="rays"></div></div><div class="rain"></div></div>');
-//     }
-//   });
-// }
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(displayWeather, error);
+} else {
+  alert('Geolocation is not supported');
+}
  
