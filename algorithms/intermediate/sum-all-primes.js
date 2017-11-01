7b3979735b03c32aa7e922d5390335fa9914ce5d
Mila Hose <mila.hose@gmail.com>
2017-11-01 13:54:47 -0700

    Add sum all primes solution

--- algorithms/intermediate/sum-all-primes.js
+++ algorithms/intermediate/sum-all-primes.js
@@ -5,16 +5,25 @@
  * The provided number may not be a prime.
  */
 
-function dropElements(arr, func) {
-  let temp = arr.slice();
-  
-  for (let i = 0; i < arr.length; i++) {
-    if (func(arr[i])) {
-      return temp;
-    } else {
-      temp = temp.slice(1);
+function sumPrimes(num) {
+  let sum = 0;
+
+  function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+	  if (num % i === 0) {
+	    return false;
+      }
+    }
+
+    return true;
+  }
+
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+	  sum += i;
     }
   }
-    
-  return [];
+
+  return sum;
 }
+  
