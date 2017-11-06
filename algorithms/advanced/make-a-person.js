81beab8ba745b3360ca30038d14b14e5b1085d40
Mila Hose <mila.hose@gmail.com>
2017-11-06 10:10:41 -0800

    Add make a person

---
+++ algorithms/advanced/make-a-person.js
@@ -0,0 +1,20 @@
+/* 
+ * Fill in the object constructor with the following methods below:
+ *    getFirstName()
+ *    getLastName()
+ *    getFullName()
+ *    setFirstName(first)
+ *    setLastName(last)
+ *    setFullName(firstAndLast)
+ *    Run the tests to see the expected output for each method.
+ * The methods that take an argument must accept only one argument and it has to be a string.
+ * These methods must be the only available means of interacting with the object.
+ */
+
+var Person = function(firstAndLast) {
+  // Complete the method below and implement the others similarly
+  this.getFullName = function() {
+    return "";
+  };
+  return firstAndLast;
+};
