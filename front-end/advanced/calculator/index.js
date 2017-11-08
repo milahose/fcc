a4eac3ff0cec52e061f7d6bdd4a4679168ef653c
Mila Hose <mila.hose@gmail.com>
2017-11-08 08:59:29 -0800

    Add operator functions

--- front-end/advanced/calculator/index.js
+++ front-end/advanced/calculator/index.js
@@ -1,10 +1,89 @@
 $(() => {
   let input = '';
-  
+  let result = [];
+
+  $('.num').on('click', (e) => {
+    let target = e.target;
+    console.log('target', $(target).val());
+    input += $(target).val();
+  });
+
+  // Add key press functionality w/ backspace functionality
+
+  // $('.operator').on('click', (e) => {  
+  //   let target = e.target;
+  //   console.log('target', $(target).val());
+  // });
+
+  $('#percent').on('click', (e) => {
+    input = input.split('')
+    input.splice(-2, 0, '.');
+    input = input.join('');
+  });
+
+  $('#float').on('click', (e) => {
+    input += '.';
+  });
+
+  $('#negative').on('click', (e) => {
+    if (input.length > 0) {
+      input = `-${input.slice()}`;
+    } else {
+      input = '-';
+    } 
+  });
+
+  $('#add').on('click', () => {
+    result.push(input);
+    if (result.length > 0) result.push('+');
+    input = '';
+  });
+
+  $('#subtract').on('click', () => {
+    result.push(input);
+    if (result.length > 0) result.push('-');
+    input = '';
+  });
+
+  $('#multiply').on('click', () => {
+    result.push(input);
+    if (result.length > 0) result.push('*');
+    input = '';
+  });
+
+  $('#divide').on('click', () => {
+    result.push(input);
+    if (result.length > 0) result.push('/');
+    input = '';
+  });
+
+  $('#equals').on('click', () => {
+    if (input.length > 0) {
+      result.push(input);
+      console.log('the result is ', eval(result.join('')));
+    }
+  });
+
   $('button').on('click', (e) => {
     e.preventDefault();
-    const $this = $(e.target);
+    let target = e.target;
 
-    console.log($this.text())
+    if ($(target).val() !== 'clear' && $(target).val() !== '%') {
+      $('#newVal').text(`${$(target).val()}`);
+      console.log('input', input)
+      if (input.length < 15) {
+        $('#newVal').text(`${input}`);
+      } else {
+        $('#newVal').text(`${input.slice(0, 14)}`);
+      }
+    }    
+
+    $('#ac').text('C');
+    $('#ac').on('click', () => {
+      $('#ac').text('AC');
+      $('#newVal').text('0');
+      input = '';
+      result = [];
+    })
   });
 });
