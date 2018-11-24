5f2894c3be69f934fa522086d0bc18fdd32296e8
Mila Hose <mila.hose@gmail.com>
2018-11-24 15:00:23 -0800

    Add html exercises

--- algorithms/intermediate/spinal-tap-case.js
+++
@@ -1,7 +0,0 @@
-/* 
- * Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
- */
-
-function spinalCase(str) {
-  return str.split(/\s|_|(?=[A-Z])/).join('-').toLowerCase();
-}
